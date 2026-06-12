2ffd8ef3fccbe5da86c7effd17551d2a364b0a1a
plnartey <163656111+plnartey@users.noreply.github.com>
2026-06-12 10:39:14 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 # A simple Python app for the Cloud CI Pipeline
 
 def main():
-    print("Cloud CI Pipeline Running")
+    print"Cloud CI Pipeline Running"
 
 if __name__ == "__main__":
     main()
